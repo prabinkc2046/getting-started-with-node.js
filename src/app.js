d2472e21f3ec5cb6462d8de8714d6694ac5e49b7
Prabin K C <pkmiracle36@gmail.com>
2024-02-15 20:12:28 +1100

    apis

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express();
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const Customer = require('./models/customer');
+const Customer = require('./models/customers');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 
@@ -12,63 +12,88 @@ if (process.env.NODE_ENV !== 'production'){
 PORT = process.env.PORT || 3000
 conn = process.env.CONN
 
-const customer = new Customer({
-    name: "hari",
-    industry: "Barista"
-})
-
-app.get("/", (req, res) => {
-    res.json({
-        message: "customer received",
-        customer
-    })
-    customer.save();
-});
-
-// To retrieve data from customer collection
-app.get('/api/customers', async(req, res) => {
-    try {
-        const customers = await Customer.find();
-        const customer_name = customers.map(customer => customer.name);
-        res.json({
-            customer_name: customer_name
+//api end point to send customer data
+app.post("/api/customers", async (req, res)=>{
+    try{
+        const {name, industry} = req.body;
+        const newCustomer = new Customer({
+        name: name,
+        industry: industry
+        });
+        await newCustomer.save();
+        res.status(201).json({
+            message: "new customer created",
+            newCustomer
         })
-    } catch(e) {
-        res.status(500).json({Error: e.message})
+    } catch(error){
+        res.status(500).json({
+            Error: error.message
+        })
     }
 })
 
-// Route to handle customer data
-app.post("/api/customers", async (req, res) => {
-    try {
-        // Extract the data from request body
-        const { name, industry } = req.body;
-
-        // Create a new instance of customer
-        const customer = new Customer({
-            name: name,
-            industry: industry
+// api end point to get all customers data
+app.get("/api/customers", async(req, res)=>{
+    try{
+        const customers = await Customer.find();
+        res.status(200).json({
+            message: "customers",
+            customers
+        });
+    } catch(e){
+        res.status(500).json({
+            Error: e.messsage
         })
+    }
+})
 
-        // save the customer data to database
-        await customer.save();
-        
-        // send response to the user
-        res.status(201).json({
-            "Customer created successfully": customer
-        })
-    } catch (e) {
-        res.json({
+// api end point to update name and industry
+app.put("/api/customers/:customerId", async(req, res)=>{
+    try{
+        const {customerId} = req.params;
+        const updatedCustomer = await Customer.findByIdAndUpdate(customerId, req.body, {new: true});
+        if (!updatedCustomer){
+            return res.status(404).json({Error:"No record found"})
+        }
+        res.status(200).json({updatedCustomer})
+    }catch(e){
+        res.status(500).json({Error:e.message});
+    }
+});
+
+//api end point to update industry only
+app.patch("/api/customers/update-industry/:customerId", async(req, res)=>{
+    const customerId = req.params.customerId;
+    const {industry} = req.body;
+    const updatedIndustry = await Customer.findByIdAndUpdate(customerId, {industry:industry}, {new: true});
+    if(!updatedIndustry){
+        return res.status(404).json({Error: "not found"})
+    }
+    res.status(200).json({updatedIndustry});
+})
+
+//api end point to get a customer
+app.get("/api/customers/customer/:customerId", async(req, res)=>{
+    try{
+        const {customerId} = req.params;
+        const customer = await Customer.findById(customerId);
+        if (!customer){
+            return res.status(404).json({
+                Error: "Record not found"
+            })
+        }
+        res.status(200).json({customer});
+    }catch(e){
+        res.status(500).json({
             Error: e.message
         })
     }
-})
-
+});
 
 const start = async () => {
     await mongoose.connect(conn);
     app.listen(PORT, () => {
-        console.log("Server is running on porT", PORT)
+        console.log("Server is running on port", PORT)
     })
 }
 
